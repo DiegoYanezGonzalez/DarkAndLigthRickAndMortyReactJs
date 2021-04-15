ab6b60400b7830a4d66d88c99d37b943c6ba0afa
diego <diego.y8@gmail.com>
2021-04-15 09:25:26 -0400

    ListImage

--- src/App.js
+++ src/App.js
@@ -1,13 +1,23 @@
-
-import './App.css';
+import React, { useState } from 'react';
 import Header from './components/Header';
 import Characters from './components/Characters';
+import './App.css';
 
 function App() {
+  const [darkMode, setDarkMode] = useState(false);    
+  const handleClick = () => {
+      setDarkMode(!darkMode);
+  }
+    
   return (
-    <div className="App">
-      <Header/>
-      <Characters/>
+    <div className={`App ${darkMode ? 'dark-mode' : 'light-mode'}`}>
+      <Header />
+      <div>
+        <button type="button" onClick={handleClick}>
+          { darkMode ? 'Change to LightMode' : 'Change to DarkMode' }
+        </button>
+      </div>  
+      <Characters />
     </div>
   );
 }
--- src/components/Characters.jsx
+++ src/components/Characters.jsx
@@ -1,28 +1,68 @@
-import React, {useState,useEffect} from 'react';
+import React, { useState, useEffect, useReducer } from 'react'
+
+const initialState={
+    favorites:[]
+}
+
+const favoritesReducer =(state,action ) =>{
+    switch(action.type){
+        case 'ADD_TO_FAVORITE':
+            return{
+                ...state,
+                favorites: [...state.favorites, action.payLoad]
+            };
+            default:
+                return state;  
+    }
+}
 
 const Characters = () => {
-//constante que desestructura los dos elementos que necesitamos
-const [characters,setCharacters] = useState([]);
 
-//le pasaremos dos parametros la funcion anonima donde esta la logica 
-//y una variable que escucha si hay un cambio 
-// tiene un arreglo vacio de esta forma solo hara render la primera vez
-useEffect(() =>{
-   fetch('https://rickandmortyapi.com/api/character/')
-   .then(response => response.json())
-   .then(data => setCharacters(data.results))
-}, []);
+    const[favorites,dispatch] = useReducer(favoritesReducer,initialState);
+    const [characters, setCharacters] = useState([]);
 
+    const getCharacters = async () => {
+        const response = await fetch('https://rickandmortyapi.com/api/character/');
+        const data = await response.json();
+        const results = data.results;
+        setCharacters(results);
+        console.log('results ->', results);
+    }
+
+    useEffect(() => {
+        getCharacters();
+    }, [])
+
+    const handleClick = favorite => {
+        dispatch({type: 'ADD_TO_FAVORITE', payload:favorite})
+    }
 
     return (
-        <div className="characters">
-          {characters.map(character =>  (
-            <h2>
-                  {character.name}
-            </h2>
-            ))}
+        <div className="">   
+            <div className="Characters">
+                {
+                    characters.map( (character) => (
+                        <div className="Character" key={character.name}>
+                            <h2>{ character.name}</h2>
+                            <figure>
+                                <img src={character.image} alt={character.species} />
+                                <figcaption>
+                                    <p><b>Gender: </b> {character.gender}</p>
+                                    <p><b>Origin: </b> {character.origin.name}</p>
+                                    <p><b>Location: </b> {character.location.name}</p>
+                                    <p><b>Specie: </b> {character.species}</p>
+                                    <p><b>Status: </b> {character.status}</p>
+                                    
+                                </figcaption>
+                                <button type="button" onClick={() =>handleClick (character)} > 
+                                    Agregar a favoritos</button>
+                            </figure>
+                        </div>
+                    ))
+                }
+            </div>
         </div>
     );
-}
+};
 
 export default Characters;
